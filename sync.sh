9543b1c59f0f91f8733d2aa4a5787c23a76d81ff
Philipp Thaler <thalerphilipp1@gmail.com>
2019-01-13 22:39:34 +0100

    Added ranger configs

--- sync.sh
+++ sync.sh
@@ -6,7 +6,8 @@ rsync -rv $HOME/.zshrc $DOTFILES
 rsync -rv $HOME/.config/polybar $DOTFILES/.config
 rsync -rv $HOME/.config/urxvtconfig $DOTFILES/.config
 rsync -rv $HOME/.config/neofetch $DOTFILES/.config
-rsync -rv $HOME/.layouts $DOTFILES/.layout
+rsync -rv $HOME/.config/ranger $DOTFILES/.config
+rsync -rv $HOME/.layouts $DOTFILES/.layouts
 rsync -rv $HOME/sync.sh $DOTFILES
 rsync -rv $HOME/restore.sh $DOTFILES
 
